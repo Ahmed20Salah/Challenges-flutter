a0fce7bb928065c33a2cf861456d7a1acc51478e
Ahmed Salah <38469778+Ahmed20Salah@users.noreply.github.com>
2024-03-10 16:05:09 +0200

    new flow

--- lib/module/auth/controller/signup_controller.dart
+++ lib/module/auth/controller/signup_controller.dart
@@ -1,29 +1,40 @@
-
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:get/get.dart';
+import 'package:global_online/module/auth/view/otp_signup_screen.dart';
 
 import '../../../core/routes/app_routes.dart';
 import '../../../core/utils/error_toast.dart';
 import '../data/data_source/auth_data_source.dart';
 
-class SignUpController extends GetxController{
+class SignUpController extends GetxController {
   late TapGestureRecognizer signUpRecognizer;
   final IAuthDataSource _authDataSource;
 
   SignUpController(this._authDataSource);
 
-  final TextEditingController numberRegisterController = TextEditingController();
+  final TextEditingController numberRegisterController =
+      TextEditingController();
   final TextEditingController nameRegisterController = TextEditingController();
   final TextEditingController emailRegisterController = TextEditingController();
-  final TextEditingController passwordRegisterController = TextEditingController();
-  final TextEditingController passwordConfirmRegisterController = TextEditingController();
+  final TextEditingController passwordRegisterController =
+      TextEditingController();
+  final TextEditingController passwordConfirmRegisterController =
+      TextEditingController();
+  final TextEditingController otpController = TextEditingController();
+  String errorText = '';
+  String sendCode = '';
+  String oTPCode = '';
+  RxString verificationIdUser = ''.obs;
 
-  Future<void> register(
-      String name, String password, String email, String confirmPassword, String phone) async {
-    final registerModel =
-    await _authDataSource.register(name,email, password, confirmPassword, phone);
+  Future<void> register() async {
+    final registerModel = await _authDataSource.register(
+        nameRegisterController.text,
+        emailRegisterController.text,
+        passwordRegisterController.text,
+        passwordConfirmRegisterController.text,
+        numberRegisterController.text);
     registerModel.fold((l) => errorToast(l.message), (r) async {
       Get.toNamed(AppRoutes.login);
     });
@@ -38,6 +49,7 @@ class SignUpController extends GetxController{
       };
     super.onInit();
   }
+
   bool showPass = true;
   bool showConfPass = true;
 
@@ -51,6 +63,51 @@ class SignUpController extends GetxController{
     update();
   }
 
+  Future<void> verifyPhone(String phone) async {
+    try {
+      await FirebaseAuth.instance.verifyPhoneNumber(
+        phoneNumber: phone,
+        verificationCompleted: (PhoneAuthCredential credential) async {
+          print('Ver credentials$credential');
+          await FirebaseAuth.instance
+              .signInWithCredential(credential)
+              .then((value) async {
+            if (value.user != null) {
+              register();
+            }
+          });
+        },
+        verificationFailed: (FirebaseAuthException e) {
+          errorToast(e.message ?? 'Error');
+        },
+        codeSent: (String verificationID, int? resendToken) {
+          verificationIdUser.value = verificationID;
+          Get.to(const OtpSignupScreen());
+        },
+        codeAutoRetrievalTimeout: (String verificationID) {
+          verificationIdUser.value = verificationID;
+        },
+      );
+    } catch (e) {}
+  }
+
+  Future<void> verifyOTP(String otp) async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithCredential(PhoneAuthProvider.credential(
+              verificationId: verificationIdUser.value, smsCode: otp))
+          .then((value) async {
+        if (value.user != null) {
+
+          register();
+        }
+      });
+    } catch (e) {
+      errorToast(e.toString());
+      Get.back();
+    }
+  }
+
   @override
   void onClose() {
     super.onClose();
@@ -60,5 +117,4 @@ class SignUpController extends GetxController{
     passwordRegisterController.dispose();
     passwordConfirmRegisterController.dispose();
   }
-
 }
---
+++ lib/module/auth/view/otp_signup_screen.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:global_online/core/resources/resource.dart';
+import 'package:global_online/core/widgets/custom_elevated_button_widget.dart';
+import 'package:global_online/module/auth/controller/signup_controller.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+
+import '../controller/login_controller.dart';
+
+class OtpSignupScreen extends StatelessWidget {
+  const OtpSignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var otp;
+    return  Scaffold(
+      backgroundColor: ColorManager.background,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            PositionedDirectional(
+                end: 0,
+                top: 0,
+                child: SvgPicture.asset(ImageAssets.intersectIcon)),
+            Container(
+              margin: EdgeInsetsDirectional.only(
+                start: AppSize.s40.w,
+                end: AppSize.s40.w,
+              ),
+              child: GetBuilder<SignUpController>(
+
+                builder: (logic) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 145,),
+                      Text(AppStrings.verifyAccount.toUpperCase(),
+                        style: getBlackItalicStyle(
+                            color: ColorManager.blackText,
+                            fontSize: FontSize.s30.sp
+                        ),
+                      ),
+                      SizedBox(height: 14.h,),
+                      Text('Please enter the 6 digit code sent to your Phone',
+                        style: getMediumStyle(color: ColorManager.greyText,
+                            fontSize: FontSize.s16.sp
+                        ),),
+                      SizedBox(height: 60.h,),
+
+
+                      Directionality(
+                        textDirection: TextDirection.ltr,
+                        child: PinCodeTextField(
+                          length: 6,
+                          controller: logic.otpController,
+                          obscureText: false,
+                          backgroundColor: Colors.transparent,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          animationType: AnimationType.fade,
+                          autoDisposeControllers: false,
+                          pinTheme: PinTheme(
+                            shape: PinCodeFieldShape.box,
+                            fieldHeight: 45.sp,
+                            fieldWidth: 45.sp,
+                            borderRadius: BorderRadius.circular(10),
+                            borderWidth: 0,
+                            fieldOuterPadding: EdgeInsets.symmetric(horizontal: 5),
+                            errorBorderColor:Colors.white,
+                            activeColor: Colors.white,
+                            activeFillColor:Colors.white,
+
+                            selectedColor: Colors.white,
+                            selectedFillColor:Colors.white,
+                            inactiveColor:Colors.white,
+                            inactiveFillColor: Colors.white,
+                          ),
+
+                          textStyle: const TextStyle(color: Colors.black),
+                          animationDuration: const Duration(milliseconds: 300),
+
+                          keyboardType: TextInputType.number,
+                          onCompleted: (v) {},
+                          onSubmitted: (code){
+                             otp = code;
+                             logic.verifyOTP(otp);
+                          },
+                          onChanged: (value) {
+                            /*logic.otpController.text =
+                                        replaceArabicNumber(value);
+                                    logic.otpController.selection =
+                                        TextSelection.fromPosition(TextPosition(
+                                            offset:
+                                            logic.otpController.text.length));
+                                    */
+                            logic.sendCode = logic.otpController.text;
+                            logic.errorText = "";
+                            logic.update();
+                          },
+                          beforeTextPaste: (text) {
+                            return true;
+                          },
+                          appContext: context,
+                        ),
+                      ),
+                      RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(children: [
+                            TextSpan(
+                                text: AppStrings.didntrecive,
+                                style: getRegularStyle(
+                                    color: ColorManager.black.withOpacity(0.7),
+                                    fontSize: FontSize.s14.sp),
+                                children: [
+                                  TextSpan(
+                                    text: AppStrings.resendCode.toUpperCase(),
+                                    recognizer: logic.signUpRecognizer,
+                                    style: getSemiBoldStyle(
+                                        color: Color(0xff36256E),
+                                        fontSize: FontSize.s14.sp),
+                                  )
+                                ]),
+                          ])),
+                      SizedBox(height: AppSize.s14.h,),
+                      Text('By continuing you’re indicating that you accept our Terms of Use and our Privacy Policy',
+                      
+                      style: getMediumStyle(color: Color(0xff9EA4B1)),
+                      ),
+                      SizedBox(height: 53.h,),
+                      CustomElevatedButton(title: 'Verify'.toUpperCase() ,
+                        titleFontStyle: getExtraBoldItalicStyle(
+                            color: ColorManager.white, fontSize: FontSize.s16
+                            .sp),
+                        onPressed: (){
+                          logic.verifyOTP(logic.otpController.text);
+
+                        },
+
+                      )
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
